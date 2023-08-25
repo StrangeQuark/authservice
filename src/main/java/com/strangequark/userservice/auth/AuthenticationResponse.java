7148cc499acaac5c0c7625fdc01cfffc8f614ba3
StrangeQuark <zacharyzirkle@hotmail.com>
2023-08-25 18:35:53 -0500

    Remove unused imports

--- src/main/java/com/strangequark/userservice/auth/AuthenticationResponse.java
+++ src/main/java/com/strangequark/userservice/auth/AuthenticationResponse.java
@@ -1,10 +1,6 @@
 package com.strangequark.userservice.auth;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
