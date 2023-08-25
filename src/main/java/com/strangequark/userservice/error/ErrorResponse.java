7c0da2dc60564deb0cea9df536ac89051cf4adfa
StrangeQuark <zacharyzirkle@hotmail.com>
2023-08-24 21:51:18 -0500

    Create ErrorResponse

---
+++ src/main/java/com/strangequark/userservice/error/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.strangequark.userservice.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private String errorMessage;
+}
