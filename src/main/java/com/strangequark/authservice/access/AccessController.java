ba75ddf26309dd55a16dacf3b81f0c8302ec6aa9
StrangeQuark <zacharyzirkle@hotmail.com>
2024-07-04 11:03:53 -0500

    Optimize package imports

--- src/main/java/com/strangequark/authservice/access/AccessController.java
+++ src/main/java/com/strangequark/authservice/access/AccessController.java
@@ -1,9 +1,10 @@
 package com.strangequark.authservice.access;
 
-import com.strangequark.authservice.auth.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 /**
