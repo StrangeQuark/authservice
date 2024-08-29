d05d6a3f79d138f63a8d108fa36a0eefcbfda45e
StrangeQuark <zacharyzirkle@hotmail.com>
2024-08-29 13:39:03 -0500

    Add /auth to the health controller

--- src/main/java/com/strangequark/authservice/config/SecurityConfig.java
+++ src/main/java/com/strangequark/authservice/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
 
         httpSecurity
                 .csrf().disable()//Disable CSRF
-                .authorizeHttpRequests().requestMatchers("/auth/**", "/health/**", "/user/verify-user-and-send-email").permitAll()//List of strings (URLs) which are whitelisted and don't need to be authenticated
+                .authorizeHttpRequests().requestMatchers("/auth/**", "/auth/health/**", "/user/verify-user-and-send-email").permitAll()//List of strings (URLs) which are whitelisted and don't need to be authenticated
                 .anyRequest().authenticated()//All other requests need to be authenticated
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//Spring will create a new session for each request
--- src/main/java/com/strangequark/authservice/health/HealthController.java
+++ src/main/java/com/strangequark/authservice/health/HealthController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * {@link RestController} responsible for health check
  */
 @RestController
-@RequestMapping("/health")
+@RequestMapping("/auth/health")
 @RequiredArgsConstructor
 public class HealthController {
     /**
