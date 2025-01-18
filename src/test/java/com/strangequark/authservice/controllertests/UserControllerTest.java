36f0b679e736ff877a4986491618bfafab70e5a7
StrangeQuark <zacharyzirkle@hotmail.com>
2025-01-18 13:10:30 -0600

    Remove broken Controller test

--- src/test/java/com/strangequark/authservice/controllertests/UserControllerTest.java
+++ src/test/java/com/strangequark/authservice/controllertests/UserControllerTest.java
@@ -1,43 +1,9 @@
-package com.strangequark.authservice.user;
+package com.strangequark.authservice.controllertests;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.strangequark.authservice.config.JwtService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.strangequark.authservice.user.UserController;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
 class UserControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private UserService userService;
-
-    @MockBean
-    private JwtService jwtService;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Test
-    void enableUserTest() throws Exception {
-        UpdatePasswordRequest request = new UpdatePasswordRequest("testUsername", "oldPassword123", "newPassword456");
-//        Mockito.when(userService.updatePassword(any(UpdatePasswordRequest.class)))
-//                .thenReturn((ResponseEntity) ResponseEntity.ok("Password updated successfully"));
-
-        mockMvc.perform(post("/user/update-password")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").value("Password updated successfully"));
-    }
 }
