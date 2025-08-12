a44c0d5b9d4702419a3ab6d2256579c3ed5be355
StrangeQuark <zacharyzirkle@hotmail.com>
2025-08-12 16:59:43 -0500

    Add newUsername to UserRequest

--- src/main/java/com/strangequark/authservice/auth/AuthenticationService.java
+++ src/main/java/com/strangequark/authservice/auth/AuthenticationService.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -155,7 +156,7 @@ public class AuthenticationService {
             //Return a 200 response with the JWT refresh token
             LOGGER.info("Authentication successful");
             return ResponseEntity.ok(new AuthenticationResponse(refreshToken));
-        } catch (Exception ex) {
+        } catch (AuthenticationException ex) {
             LOGGER.error(ex.getMessage());
             return ResponseEntity.status(401).body(new ErrorResponse(ex.getMessage()));
         }
